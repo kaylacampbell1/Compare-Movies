c40e06398110b5a34c5a880deaf74516dda39ef4
kaylacampbell1 <kaylacampbell3003@gmail.com>
2023-11-01 23:49:01 -0400

    used DOM to load all movie data onto screen, first chart completed

--- src/main.js
+++ src/main.js
@@ -1,3 +1,42 @@
 import './style.css'
+import movieData from './movie-data.json'
+import Chart from "chart.js/auto";
 
-console.log("Hello World");
+let moviesDiv = document.getElementById("view-movies")
+
+const displayMovies = (movieData) => {
+  for (let idx = 0; idx < movieData.length; idx++) {
+    let div = document.createElement("div");
+    div.classList.add("movie");
+    let name = document.createElement("h3");
+    let criticScore = document.createElement("p");
+    let audienceScore = document.createElement("p");
+    let domesticGross = document.createElement("p");
+    let genre = document.createElement("p");
+    name.textContent = movieData[idx].title;
+    criticScore.textContent = `Critic Score: ${movieData[idx].criticScore}%`;
+    audienceScore.textContent = `Audience Score: ${movieData[idx].audienceScore}%`;
+    domesticGross.textContent = `Domestic Total: $${movieData[idx].domestic}`;
+    genre.textContent = `Genre: ${movieData[idx].genre}`;
+    div.append(name, criticScore, audienceScore, domesticGross, genre);
+    moviesDiv.append(div);
+  }
+  
+}
+displayMovies(movieData);
+
+const displayDomesticChart = () => {
+  new Chart(document.getElementById("box-office"), {
+    type: "bar",
+    data: {
+      labels: movieData.map((movie) => movie.title),
+      datasets: [
+        {
+          label: "Domestic Gross",
+          data: movieData.map((movie) => movie.domestic),
+        },
+      ],
+    },
+  });
+}
+displayDomesticChart();
